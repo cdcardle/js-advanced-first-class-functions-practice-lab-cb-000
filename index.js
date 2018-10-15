49c9c3306c6301d2c2d495836c139fb13dc928c6
Chris Cardle <cdcardle@gmail.com>
2018-10-15 14:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const logDriverNames = function(drivers) {
   drivers.forEach(driver => console.log(driver.name))
 }
+
+const logDriversByHometown = function(drivers, town) {
+  drivers.forEach(driver => (driver.hometown === town) ? console.log(driver.name))
+}
