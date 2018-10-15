aebfa36a5870a4d9a9f4b437996a8073b6a058b5
Chris Cardle <cdcardle@gmail.com>
2018-10-15 15:37:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const driversByRevenue = function(drivers) {
 const driversByName = function(drivers) {
   let newArray = []
   drivers.forEach(function(driver) {
-    newArray.push(driver);
+    newArray.push(driver.name);
   })
   return newArray.sort();
 }
