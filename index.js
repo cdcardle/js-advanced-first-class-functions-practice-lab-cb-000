1e23b2884b9ca4eb2064bab51c6103a29170046f
Chris Cardle <cdcardle@gmail.com>
2018-10-15 15:23:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const logDriverNames = function(drivers) {
-  drivers.forEach(driver => console.log(driver.name))
+  drivers.forEach(driver => console.log(driver.name));
 }
 
 const logDriversByHometown = function(drivers, town) {
-  drivers.forEach(driver => ((driver.hometown === town) ? console.log(driver.name) : 0))
+  drivers.forEach(driver => ((driver.hometown === town) ? console.log(driver.name) : 0));
 }
 
 const driversByRevenue = function(drivers) {
-  return drivers.sort((a, b) => a.revenue - b.revenue)
+  return drivers.sort((a, b) => a.revenue - b.revenue);
 }
